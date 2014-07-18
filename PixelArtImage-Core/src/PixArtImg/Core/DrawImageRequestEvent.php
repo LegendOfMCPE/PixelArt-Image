029a1245b991c262c5e6f82e1f1ac0bbc56bcf7f
PEMapModder <pemapmodder1970@gmail.com>
2014-07-18 22:31:14 +0800

    Update DrawImageRequestEvent.php

--- PixelArtImage-Core/src/PixArtImg/Core/DrawImageRequestEvent.php
+++ PixelArtImage-Core/src/PixArtImg/Core/DrawImageRequestEvent.php
@@ -8,11 +8,15 @@ use pocketmine\event\player\PlayerEvent;
 
 class DrawImageRequestEvent extends PlayerEvent implements Cancellable{
 	public static $handlerList = null;
-	public function __construct(array $mappedImage, Player $player){
+	public function __construct(array $mappedImage, Player $player, $ticks = 1200){
 		$this->player = $player;
 		$this->mapped = $mapped;
+		$this->ticks = $ticks;
 	}
 	public function getMapped(){
 		return $this->mapped;
+	}
+	public function getTicks(){
+		return $this->ticks;
 	}
 }
